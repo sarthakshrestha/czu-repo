e9ca7d4a9dc1a54174e8b2d8f316733b9709e64d
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-03-08 23:17:33 +0545

    styles

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -16,19 +16,26 @@ const Homepage = () => {
                     @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap')
                 `}
       </style>
-
-      <div className="navbar-left">
-        <h1>caesarinn.</h1>
+      <div className="navbar-container">
+        <div className="navbar-left">
+          <h1>caesarinn.</h1>
+        </div>
       </div>
       <div className="navbar-right">
-        <h2>folio </h2>
+        {/* <h2>folio </h2> */}
+        <h2>
+            stills
+        </h2>
+        <h2>
+            videoworks
+        </h2>
         <h2>
           <NavLink to="/prints">prints</NavLink>
         </h2>
         <h2>
           <NavLink to="/images">image</NavLink>
         </h2>
-        <h2> archives </h2>
+        {/* <h2> archives </h2> */}
       </div>
       {/* <Portfolionpm run devItem/> */}
     </nav>
