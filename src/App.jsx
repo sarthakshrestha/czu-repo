e0847fc22a82338599a89a311db31b39caf694b5
Sarthak Shrestha <sarthakstha24@gmail.com>
2024-03-08 22:35:10 +0545

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Homepage';
+import Homepage from './components/Homepage';
 import PortfolioItem from './components/canvas/Canvas';
 import PrintItem from './components/prints/prints';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/images" element={<PortfolioItem />} />
         <Route path="/prints" element={<PrintItem />} />
       </Routes>
